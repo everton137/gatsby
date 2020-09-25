ed12f068b8c3c126fea20cf27e1e0e4f6cd83e9c
Everton Zanella Alvarenga <eza@prontonmail.ch>
2020-09-25 17:24:24 +0200

    Adding Tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Olá mundo!</div>
+  return (
+    <div className="bg-red-300">
+      <div className="p-3 text-green-800 font-bold text-center">Olá mundo!</div>
+    </div>
+  )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
